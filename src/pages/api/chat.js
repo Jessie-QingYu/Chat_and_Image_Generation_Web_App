488f1396722d7801efd2062d9bdef6ce0686afb2
QingYu-NEU <yu.qing4@northeastern.edu>
2023-08-13 16:34:22 -0700

    04 - add mock_data to chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -6,18 +6,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-
-    const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        content: 'what is the first computer language?'
-      }
-    ]
-  });
-
-  res.status(200).json({
-    data: completion.data
-  })
+    const mock_data = {
+        "data": [
+          {
+            "result": "This is the mock_data, need a valid OPENAI_API_KEY and OPENAI_ORGANIZATION to generate the real data"
+          }
+        ]
+    }
+    let completion = null
+    let e = null;
+    try {
+        completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+              {
+                role: 'user',
+                content: 'what is the first computer language?'
+              }
+            ]
+        });
+        
+        res.status(200).json({
+            // data: completion.data
+            data: mock_data.data
+        })
+        
+    } catch (exception) {
+    console.log({ exception })
+    e = exception
+    }
+    res.status(200).json({
+        // data: completion.data
+        data: mock_data.data
+    })
 }

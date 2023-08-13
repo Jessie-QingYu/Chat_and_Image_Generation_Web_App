fc0696ca9ebafb6a7de2e465298de72c720f6812
QingYu-NEU <yu.qing4@northeastern.edu>
2023-08-13 13:47:38 -0700

    01 - Installing & Configuring the OpenAI SDK

--- src/pages/api/image.js
+++ src/pages/api/image.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION,
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  // console.log({openai})
+  const response = await openai?.listEngines();
+  // console.log({response});
+  res.status(200).json({ data: response?.data?.data })
 }

c813e985879148046e946bd40878f73e90efc25c
QingYu-NEU <yu.qing4@northeastern.edu>
2023-08-13 22:41:42 -0700

    Revert "06 - Formatting Chat Completions as JSON"
    This reverts commit b03a9067b9644ee23fe85be9f56c1f956eeaf93c.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Get Started with AI-Driven App Development Using the OpenAI Node.js SDK
-
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head'
-// import { createParser } from 'eventsource-parser';
+import { createParser } from 'eventsource-parser';
 
 import Layout from '@/components/Layout';
 import Section from '@/components/Section';
@@ -20,7 +20,6 @@ function getFieldFromFormByName({ name, form } = {}) {
 
 export default function Home() {
   const [text, setText] = useState();
-  const [attributes, setAttributes] = useState();
   const [image, setImage] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -33,52 +32,47 @@ export default function Home() {
     });
 
     setIsLoading(true);
-    // setText(undefined);
-    setAttributes(undefined);
+    setText(undefined);
 
-    // const response = await fetch('/api/chat-stream', {
-    const { data } = await fetch('/api/attributes', {
+    const response = await fetch('/api/chat-stream', {
       method: 'POST',
       body: JSON.stringify({
         prompt
-        //   }),
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //   },
-        // });
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-        // const reader = response.body.getReader();
-        // const decoder = new TextDecoder();
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
 
-        // function onParse(event) {
-        //   if (event.type === 'event') {
-        //     try {
-        //       const data = JSON.parse(event.data);
-        //       data.choices
-        //         .filter(({ delta }) => !!delta.content)
-        //         .forEach(({ delta }) => {
-        //           setText(prev => {
-        //             return `${prev || ''}${delta.content}`;
-        //           })
-        //         });
-        //     } catch (e) {
-        //       console.log(e)
-        //     }
-        //   }
-        // }
+    function onParse(event) {
+      if (event.type === 'event') {
+        try {
+          const data = JSON.parse(event.data);
+          data.choices
+            .filter(({ delta }) => !!delta.content)
+            .forEach(({ delta }) => {
+              setText(prev => {
+                return `${prev || ''}${delta.content}`;
+              })
+            });
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    }
 
-        // const parser = createParser(onParse)
+    const parser = createParser(onParse)
 
-        // while (true) {
-        //   const { value, done } = await reader.read();
-        //   const dataString = decoder.decode(value);
-        //   if (done || dataString.includes('[DONE]')) break;
-        //   parser.feed(dataString);
-        // }
-      })
-    }).then(r => r.json());
+    while (true) {
+      const { value, done } = await reader.read();
+      const dataString = decoder.decode(value);
+      if (done || dataString.includes('[DONE]')) break;
+      parser.feed(dataString);
+    }
 
-    setAttributes(data.attributes);
     setIsLoading(false);
   }
 
@@ -124,10 +118,7 @@ export default function Home() {
               <Button disabled={isLoading}>Generate</Button>
             </FormRow>
           </Form>
-          {/* {text && <p>{text}</p>} */}
-          {attributes && attributes.map(attribute => {
-            return <p key={attribute}>{ attribute }</p>
-          })}
+          {text && <p>{text}</p>}
         </Container>
       </Section>
       <Section>
